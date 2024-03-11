1a578c818597e32af090c900d111ff50794aac4b
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-03-11 20:38:17 -0300

    Update Exercicio04.java

--- Aula04 - Operadores Lógicos & Exercícios sobre Estruturas de Seleção/Exercicio04.java
+++ Aula04 - Operadores Lógicos & Exercícios sobre Estruturas de Seleção/Exercicio04.java
@@ -1,7 +1,7 @@
 /*
-ExercÃ­cio 4: Escreva um programa que pede para o usuÃ¡rio inserir dois nÃºmeros, obtÃ©m os nÃºmeros do usuÃ¡rio
-e entÃ£o imprime o maior nÃºmero seguido pelas palavras â€œÃ© o maiorâ€.
-Se os nÃºmeros forem iguais, imprime a mensagem â€œEstes nÃºmeros sÃ£o iguaisâ€.
+Exerci­cio 4: Escreva um programa que pede para o usuario inserir dois numeros, obtem os numeros do usuario
+e entao imprime o maior numero seguido pelas palavras "eh o maior".
+Se os numeros forem iguais, imprime a mensagem "Estes nUmeros sAo iguais".
 */
 
 import javax.swing.JOptionPane;
@@ -9,15 +9,15 @@ import javax.swing.JOptionPane;
 public class Exercicio04 {
 
     public static void main(String[] args) {
-        double numero1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro nÃºmero: "));
-        double numero2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo nÃºmero: "));
+        double numero1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+        double numero2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo numero: "));
         
         if (numero1 > numero2) {
-            JOptionPane.showMessageDialog(null, numero1 + " Ã© o maior.");
+            JOptionPane.showMessageDialog(null, numero1 + " eh o maior.");
         } else if (numero2 > numero1) {
-            JOptionPane.showMessageDialog(null, numero2 + " Ã© o maior.");
+            JOptionPane.showMessageDialog(null, numero2 + " eh o maior.");
         } else {
-            JOptionPane.showMessageDialog(null, "Estes nÃºmeros sÃ£o iguais.");
+            JOptionPane.showMessageDialog(null, "Estes numeros sao iguais.");
         }
     }
 }
