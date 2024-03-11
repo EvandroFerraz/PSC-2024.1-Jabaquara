852f4a549872b50bf1e481df849b47625216d68d
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-03-11 20:35:20 -0300

    Update Exercicio02.java

--- Aula04 - Operadores Lógicos & Exercícios sobre Estruturas de Seleção/Exercicio02.java
+++ Aula04 - Operadores Lógicos & Exercícios sobre Estruturas de Seleção/Exercicio02.java
@@ -1,6 +1,6 @@
 /*
-ExercÃ­cio 2: Escreva um programa que faz a leitura de dois nÃºmeros reais e verifica se eles sÃ£o iguais.
-Se forem, o programa deve mostrar uma mensagem ao usuÃ¡rio informando-o disso. Caso contrÃ¡rio, o programa somente termina.
+Exerci­cio 2: Escreva um programa que faz a leitura de dois numeros reais e verifica se eles sao iguais.
+Se forem, o programa deve mostrar uma mensagem ao usuario informando-o disso. Caso contario, o programa somente termina.
 */
 
 import javax.swing.JOptionPane;
@@ -8,11 +8,11 @@ import javax.swing.JOptionPane;
 public class Exercicio02 {
 
     public static void main(String[] args) {
-        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro nÃºmero: "));
-        double num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo nÃºmero: "));
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro numero: "));
+        double num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo numero: "));
         
         if (num1 == num2) {
-            JOptionPane.showMessageDialog(null, "Os nÃºmeros sÃ£o iguais.");
+            JOptionPane.showMessageDialog(null, "Os numeros sao iguais.");
         }
     }
 }
