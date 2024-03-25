8f3bf0832f72200ad9cedba1f5a9581f8a2a403d
Evandro Ferraz <ferraz.evandro@outlook.com>
2024-03-24 23:03:40 -0300

    Create Carro.java

---
+++ Aula06 - Orientação a Objetos: Classes, Métodos e Atributos/Exercicios/Carro.java
@@ -0,0 +1,40 @@
+public class Carro {
+    private int velocidade;
+    private String modelo;
+    
+    public Carro(int velocidade, String modelo){
+        this.velocidade = velocidade;
+        this.modelo = modelo;
+    }
+    
+    public void acelerar(int acrescimo){
+        this.velocidade = this.velocidade + acrescimo;
+    }
+    
+    public void desacelerar(int decrescimo){
+        this.velocidade = this.velocidade - decrescimo;
+    }
+    
+    public void verificarLimiteVelocidade(){
+        if(this.velocidade > 100){
+            System.out.println("Você está acima do limite");
+        }else{
+            System.out.println("Você está dentro do limite");
+        }
+    }
+    
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    public int getVelocidade() {
+        return velocidade;
+    }
+    public String getModelo() {
+        return modelo;
+    }     
+}
